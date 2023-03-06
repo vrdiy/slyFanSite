3157642e9f1bae763537518d2fcbdc44fd171aaa
vrdiy <amverdigo@gmail.com>
2023-03-05 23:57:46 -0500

    more setup

--- slyProj/settings.py
+++ slyProj/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user',
+    'slysite'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
